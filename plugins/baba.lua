1de0dabe3e309bbb1e04416a77966d8a863e7aa2
crazyboy11 <vito.vcp@outlook.com>
2016-06-09 16:19:15 +0430

    Create baba.lua

---
+++ plugins/baba.lua
@@ -0,0 +1,19 @@
+do
+
+function run(msg, matches)
+  return [[ با بابام چیکار داری دیوث :| ]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^[!/]حامد$",
+    "^[!/]crazy boy$",
+    "^[Vv]@crazyboy10$",
+    "^[Vv]کریزی$"
+  }, 
+  run = run 
+}
+
+end
