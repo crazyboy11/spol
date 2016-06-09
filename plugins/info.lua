e32fe1e4facd75f87e6c7016629668b43925653c
crazyboy11 <vito.vcp@outlook.com>
2016-06-09 16:12:50 +0430

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -1,5 +1,5 @@
 do
-local ali_ghoghnoos = 85831686 --put your id here(BOT OWNER ID)
+local ali_ghoghnoos = 172178919 --put your id here(BOT OWNER ID)
 
 local function setrank(msg, name, value) -- setrank function
   local hash = nil
